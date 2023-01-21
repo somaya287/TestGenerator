46c64be648d7d500d784408c220dd85a3497b6f9
Hajar <hajar.dami.14@gmail.com>
2023-01-21 01:47:17 +0100

    some changes

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -6,15 +6,15 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
+import AddDiagrammesService from "../services/AddDiagrammeService"
 
-
-
+import Button from '@mui/material/Button';
 
 export default function  Title () {
-      const [age, setAge] = React.useState('');
+      const [diagramme, setDiagramme] = React.useState('');
 
       const handleChange = e => {
-        setAge(e.target.value);
+        setDiagramme(e.target.value);
         
     };
     const styles = theme => ({
@@ -24,22 +24,29 @@ export default function  Title () {
         }
       });
       
+      const handleSubmit = (event) => {
 
+        AddDiagrammesService (event,diagramme)
+    };
     return( <div>
             <img  className="imgz" src="./Logo.png" />
 
     <p className="text">PASSWORD GENERATOR</p> 
 
-    <Box sx={{ minWidth: 120 }}>
-    <label className='txtField' style={{color:"white"}}for="standard-select">Select Diagram type</label>
+    <Box sx={{ minWidth: 120 }} onSubmit={handleSubmit}>
+    <label className='txtField1' style={{color:"white"}}for="standard-select">Select Diagram type</label>
 
-   <select className='txtField' name="country" id="country" placeholder="Select Diagram type" label="Select Diagram type">
-            <option value="Diagramme de Séquence">Diagramme de Séquence</option>
+   <select  onChange={handleChange} 
+ className='txtField' name="Diagramme" id="Diagramme"  >
+            <option value="Diagramme de Séquence" style={{color:"white"}} selected>Diagramme de Séquence</option>
             <option value="Diagramme de Séquence">Diagramme d'activité</option>
-            
-        </select>
+           
 
-        <input className='txtinpt' type="file" name="country" id="country" placeholder="Select Diagram type"  ></input>
+    </select>
+    <label  className='txtField2' style={{color:"white"}}for="standard-select">Upload your diagrame(.io)</label>
+
+        <input id="file" className='txtinpt' type="file" name="country"  placeholder="Select Diagram type"  ></input>
+      
     </Box>
     </div>
     );
---
+++ frontend/src/services/AddDiagrammeService.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const AddDiagrammesService = (data,Diagramme) => {
+    const dataToSend = {
+        type: {Diagramme},
+        file: data.get('fie'),
+        
+    }
+    console.log("data");
+    console.log(dataToSend);
+    axios.post(`urltochange`, dataToSend, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }).then(
+        (res) => {
+console.log(res);
+          
+        }
+        ,
+        (err) => {
+            alert("erreur lors de l'authentification, veuillez reentrer vos données, en cas de besoin contacter l'admin");
+            console.error(err);
+        }
+    );
+
+};
+
+
+
+
+export default AddDiagrammesService;
