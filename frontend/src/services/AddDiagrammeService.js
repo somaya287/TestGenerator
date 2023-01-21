1f877af76cb619c5f9d8f9fbc3ed809efbb0b7e9
Hajar <hajar.dami.14@gmail.com>
2023-01-21 13:52:14 +0100

    file uplaod

--- frontend/src/services/AddDiagrammeService.js
+++ frontend/src/services/AddDiagrammeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const AddDiagrammesService = (data,Diagramme) => {
+const AddDiagrammesService = (data,Diagramme,SetresultTest) => {
     const dataToSend = {
         type: {Diagramme},
         file: data.get('fie'),
@@ -14,6 +14,7 @@ const AddDiagrammesService = (data,Diagramme) => {
         }
     }).then(
         (res) => {
+            SetresultTest(res.data)
 console.log(res);
           
         }
